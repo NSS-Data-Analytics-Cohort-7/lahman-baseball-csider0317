5b8c6037cf7ebefb77ea06a172cd62371595b2d8
csider <csider0317@gmail.com>
2022-08-30 21:09:17 -0500

    I have the managers for 9 and im pulling in teams now

--- script/lahmanbaseballcsider0317.sql
+++ script/lahmanbaseballcsider0317.sql
@@ -266,15 +266,20 @@ FROM awardsmanagers
     group by playerid, lgid, awardid) 
     SELECT 
     manager.playerid, 
-       SUM(manager.al_wins), 
-    sum(manager.nl_wins)
+       SUM(manager.al_wins)as alwin, 
+    sum(manager.nl_wins)as nlwin
     FROM manager
     WHERE lgid IN ('AL','NL')
     group by manager.playerid
     order by playerid)
     SELECT
-   
+
+tw.playerid 
+FROM tw
+where tw.alwin>0 and tw.nlwin>0
     
+   
+ 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.   
     
     
    
